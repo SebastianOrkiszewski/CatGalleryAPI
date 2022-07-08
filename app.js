8be5bdca882cd4bc1dc600beadde6bb9a66cb580
SebastianOrkiszewski <s.orkiszewski@gmail.com>
2022-07-08 14:48:21 +0200

    update 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('#btn');
+const img = document.querySelector('#cat');
+
+btn.addEventListener('click', nextCat);
+
+function nextCat() {
+    fetch('https://aws.random.cat/meow')
+    .then(res => res.json())
+    .then(data => {
+        img.innerHTML = `<img src=${data.file} alt="cat" />`
+    });
+}
+
+
